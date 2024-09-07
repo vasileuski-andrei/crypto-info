578a2ef276e0654e7473b98bb10cde40fa4a60e7
Andrei Vasileuski <vasileuski.andrei@gmail.com>
2024-09-08 02:13:21 +0300

    git init

---
+++ src/main/java/eu/senla/crypto_info/CryptoInfoApplication.java
@@ -0,0 +1,13 @@
+package eu.senla.crypto_info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/senla/crypto_info/CryptoInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.senla.crypto_info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
