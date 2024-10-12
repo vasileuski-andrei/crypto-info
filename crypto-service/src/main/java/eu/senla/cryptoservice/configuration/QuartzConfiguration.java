546619c888159389d59d4811bd987b4225020c0f
Andrei Vasileuski <vasileuski.andrei@gmail.com>
2024-10-13 02:12:54 +0300

    configure quartz job; set up docker compose

--- crypto-service/src/main/java/eu/senla/cryptoservice/configuration/QuartzConfiguration.java
+++ crypto-service/src/main/java/eu/senla/cryptoservice/configuration/QuartzConfiguration.java
@@ -1,31 +1,39 @@
 package eu.senla.cryptoservice.configuration;
 
 import eu.senla.cryptoservice.job.CoinmarketcapJob;
-import org.quartz.*;
+import org.quartz.CronTrigger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
+import org.springframework.scheduling.quartz.JobDetailFactoryBean;
+
+import java.util.Objects;
 
 @Configuration
 public class QuartzConfiguration {
 
-    @Value("${spring.quartz.cron-schedule}")
+    @Value("${quartz.cron-schedule}")
     private String quartzCronSchedule;
 
     @Bean
-    public JobDetail jobDetail() {
-        return JobBuilder.newJob(CoinmarketcapJob.class)
-                .withIdentity("myJob")
-                .storeDurably()
-                .build();
+    JobDetailFactoryBean jobDetailFactoryBean() {
+        JobDetailFactoryBean jobDetailFactoryBean = new JobDetailFactoryBean();
+        jobDetailFactoryBean.setJobClass(CoinmarketcapJob.class);
+        jobDetailFactoryBean.setName(CoinmarketcapJob.class.getName());
+        jobDetailFactoryBean.setDurability(true);
+        jobDetailFactoryBean.setRequestsRecovery(true);
+
+        return jobDetailFactoryBean;
     }
 
     @Bean
-    public Trigger jobTrigger() {
-        return TriggerBuilder.newTrigger()
-                .forJob(jobDetail())
-                .withIdentity("jobTrigger")
-                .withSchedule(CronScheduleBuilder.cronSchedule(quartzCronSchedule))
-                .build();
+    CronTriggerFactoryBean jobTrigger(JobDetailFactoryBean jobDetailFactoryBean) {
+        CronTriggerFactoryBean cronTriggerFactoryBean = new CronTriggerFactoryBean();
+        cronTriggerFactoryBean.setJobDetail(Objects.requireNonNull(jobDetailFactoryBean.getObject()));
+        cronTriggerFactoryBean.setCronExpression(quartzCronSchedule);
+        cronTriggerFactoryBean.setMisfireInstruction(CronTrigger.MISFIRE_INSTRUCTION_FIRE_ONCE_NOW);
+
+        return cronTriggerFactoryBean;
     }
 }
